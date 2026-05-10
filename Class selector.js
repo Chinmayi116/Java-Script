0b201d0cc6d1f6cf868b3a3343bddb4336614ee5
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-05-10 15:48:41 +0530

    Add HTML file with class selectors example

---
+++ Class selector.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Class Selector</title>
+
+    <style>
+        .heading {
+            color: blue;
+            font-size: 30px;
+        }
+
+        .para {
+            color: green;
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+
+    <h1 class="heading">Welcome to CSS</h1>
+
+    <p class="para">
+        This is an example of class selector.
+    </p>
+
+</body>
+</html>
