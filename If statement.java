d07da7c65c831864ac8a711a91ebacd784078709
Chinmayi Patil <chinmayipatil746@gmail.com>
2026-05-22 22:02:27 +0530

    Add temperature check with console messages

---
+++ If statement.java
@@ -0,0 +1,13 @@
+let temperature = 25;
+if (temperature > 30)
+{
+ console.log("It's hot!");
+} 
+else if (temperature < 10)
+{
+ console.log("It's cold!");
+} 
+else
+{
+ console.log("The weather is moderate.");
+}
